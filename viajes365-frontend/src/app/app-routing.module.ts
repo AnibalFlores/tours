9b7854de11692f8ebe5656aaf70282ba49d4222f
Anibal H. Flores <floresanibal@yahoo.com.ar>
2021-03-16 16:54:40 -0300

    Final Commit added login form account module and services renamed viajar to viajes

--- viajes365-frontend/src/app/app-routing.module.ts
+++ viajes365-frontend/src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
+const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'login', redirectTo: 'account/login', pathMatch: 'full' },
+  { path: 'account', loadChildren: accountModule, pathMatch: 'prefix' },
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
 ];
